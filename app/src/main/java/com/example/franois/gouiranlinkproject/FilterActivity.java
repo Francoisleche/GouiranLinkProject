8fb4d19873dc0020689915679a2db15abb7d86c3
François Meynard <meynardfrancois@gmail.com>
2017-01-31 14:29:55 +0100

    message3

---
+++ app/src/main/java/com/example/franois/gouiranlinkproject/FilterActivity.java
@@ -0,0 +1,21 @@
+package com.example.franois.gouiranlinkproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by François on 30/01/2017.
+ */
+
+public class FilterActivity extends Activity{
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_filtres);
+    }
+
+
+}

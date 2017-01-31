b83fd11554ae176bed14a35dc3cf4c7690ccf3ce
Maxime Pyram <maxime.pyram@epitech.eu>
2017-01-31 14:27:01 +0100

    Geolocalisation Autour de moi NearbyProfessionals

---
+++ app/src/main/java/com/example/franois/gouiranlinkproject/Object/NotificationsActivity.java
@@ -0,0 +1,15 @@
+package com.example.franois.gouiranlinkproject.Object;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.franois.gouiranlinkproject.R;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
